a70bd99a9367ddd17ea809a87e380d25d4ec9458
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-20 22:16:59 +0100

    add todo

--- Kluster.Shared/Domain/ApplicationUser.cs
+++ Kluster.Shared/Domain/ApplicationUser.cs
@@ -8,7 +8,8 @@ namespace Kluster.Shared.Domain
         public override string Id { get; set; } = "U-" + Guid.NewGuid().ToString();
         [MaxLength(50)] public required string FirstName { get; set; } = string.Empty;
         [MaxLength(50)] public required string LastName { get; set; } = string.Empty;
-
+        // ensure phone number and email are never null.
+        
         /// <summary>
         /// Can be either client or business.
         /// </summary>
