e0799d281376ac62fe4ee9c3b68436e83972c09d
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-21 13:50:47 +0100

    replace magic numbers with constants

--- Kluster.Shared/Domain/Business.cs
+++ Kluster.Shared/Domain/Business.cs
@@ -6,21 +6,40 @@ namespace Kluster.Shared.Domain
     {
         // todo: use fluent validation to replace data annotations.
         // prepend 'B' to Id
-        [MaxLength(AppConstants.MaxIdLength)]
-        public string Id { get; set; } = "B-" + Guid.NewGuid();
+        [MaxLength(AppConstants.MaxIdLength)] public string Id { get; set; } = "B-" + Guid.NewGuid();
 
-        [MaxLength(50)] public required string Name { get; set; }
-        [MaxLength(50)] public required string Address { get; set; }
-        [MaxLength(15)] public string? CacNumber { get; set; }
-        [MaxLength(15)] public string? RcNumber { get; set; }
+        [MaxLength(BusinessConstants.MaxNameLength)]
+        public required string Name { get; set; }
 
-        [MaxLength(200)] public string? Description { get; set; }
+        [MaxLength(BusinessConstants.MaxAddressLength)]
+        public required string Address { get; set; }
 
-        [MaxLength(20)] public required string Industry { get; set; }
+        [MaxLength(BusinessConstants.MaxCacNumberLength)]
+        public string? CacNumber { get; set; }
+
+        [MaxLength(BusinessConstants.MaxRcNumberLength)]
+        public string? RcNumber { get; set; }
+
+        [MaxLength(BusinessConstants.MaxDescriptionLength)]
+        public string? Description { get; set; }
+
+        [MaxLength(BusinessConstants.MaxIndustryLength)]
+        public required string Industry { get; set; }
 
         // navigation properties
         [MaxLength(AppConstants.MaxIdLength)] public required string UserId { get; set; }
 
         public ApplicationUser User { get; set; } = null!;
+    }
+
+    public static class BusinessConstants
+    {
+        public const int MaxCacNumberLength = 15;
+        public const int MaxRcNumberLength = 15;
+        public const int MaxAddressLength = 50;
+        public const int MaxIndustryLength = 20;
+        public const int MaxDescriptionLength = 200;
+        public const int MinNameLength = 3;
+        public const int MaxNameLength = 50;
     }
 }
