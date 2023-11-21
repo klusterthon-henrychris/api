b98ea21595e25120a311bb00192e6507e1893057
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-21 15:39:30 +0100

    Create Client.cs

---
+++ Kluster.Shared/Domain/Client.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kluster.Shared.Domain;
+
+public class Client
+{
+    [MaxLength(DomainConstants.MaxIdLength)]
+    public string Id { get; set; } = "C-" + Guid.NewGuid();
+
+    [MaxLength(DomainConstants.MaxNameLength)]
+    public required string FirstName { get; set; }
+
+    [MaxLength(DomainConstants.MaxNameLength)]
+    public required string LastName { get; set; }
+
+    [MaxLength(DomainConstants.MaxNameLength)]
+    public string? BusinessName { get; set; }
+
+    [MaxLength(DomainConstants.MaxAddressLength)] public required string Address { get; set; }
+    [MaxLength(DomainConstants.MaxEmailAddressLength)] public required string EmailAddress { get; set; }
+
+    public bool EmailConfirmed { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    [MaxLength(DomainConstants.MaxIdLength)] public required string BusinessId { get; set; }
+    public Business Business { get; set; } = null!;
+    public List<Invoice> Invoices { get; set; } = [];
+}
