09b075b114f8805e02aefa587140f4dc52e0ec34
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-21 13:48:35 +0100

    Create UserNotSetException.cs

---
+++ Kluster.Shared/Exceptions/UserNotSetException.cs
@@ -0,0 +1,7 @@
+﻿namespace Kluster.Shared.Exceptions;
+
+public class UserNotSetException(string? message) :
+    Exception(string.IsNullOrEmpty(message)
+        ? "There is no user in the JWT, or the JWT is invalid."
+        : message
+    );
