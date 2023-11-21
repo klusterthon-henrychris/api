d6e8bfa9a07738559cca844fb87d807fd7b82b8f
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-21 15:37:57 +0100

    move constants to single location

---
+++ Kluster.Shared/Domain/DomainConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace Kluster.Shared.Domain;
+
+public static class DomainConstants
+{
+    /// <summary>
+    /// Use globally to enforce ID lengths
+    /// </summary>
+    public const int MaxIdLength = 450;
+
+    public const int MinNameLength = 3;
+    public const int MaxNameLength = 50;
+
+    public const int MaxCacNumberLength = 15;
+    public const int MaxRcNumberLength = 15;
+    
+    public const int MaxDescriptionLength = 200;
+    
+    public const int MinAddressLength = 3;
+    public const int MaxAddressLength = 200;
+
+    public const int MinEmailAddressLength = 5;
+    public const int MaxEmailAddressLength = 50;
+
+    public const int MaxJsonLength = 450;
+    public const int MaxEnumLength = 20;
+    
+}
