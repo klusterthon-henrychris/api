5a616274515471a7fe4cd5c0399f03221ac3339f
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-21 17:07:21 +0100

    Create GenericErrors.cs

---
+++ Kluster.Shared/ServiceErrors/GenericErrors.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+
+namespace Kluster.Shared.ServiceErrors
+{
+    public static class GenericErrors
+    {
+        public static Error GenericError => Error.Unexpected(
+            code: "Event.GenericError",
+            description: "Sorry, something went wrong. Please reach out to an admin.");
+    }
+}
