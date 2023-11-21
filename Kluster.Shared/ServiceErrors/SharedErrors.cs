799912d6f49c346996bd4953f9d9e4c003f043b2
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-21 17:46:21 +0100

    use typeof(T) to fix generic resolution bug
    from 'T does not exist' to 'Business does not exist'

--- Kluster.Shared/ServiceErrors/SharedErrors.cs
+++ Kluster.Shared/ServiceErrors/SharedErrors.cs
@@ -6,51 +6,51 @@ namespace Kluster.Shared.ServiceErrors;
 public static class SharedErrors<T>
 {
     public static Error NotFound => Error.NotFound(
-        code: $"{nameof(T)}.NotFound",
-        description: $"The {nameof(T)} does not exist.");
+        code: $"{typeof(T).Name}.NotFound",
+        description: $"The {typeof(T).Name} does not exist.");
 
     public static Error MissingName => Error.Validation(
-        code: $"{nameof(T)}.MissingName",
-        description: $"The {nameof(T)} has no name.");
+        code: $"{typeof(T).Name}.MissingName",
+        description: $"The {typeof(T).Name} has no name.");
 
     public static Error InvalidName => Error.Validation(
-        code: $"{nameof(T)}.InvalidName",
-        description: $"{nameof(T)} name must be at least {DomainConstants.MinNameLength}" +
+        code: $"{typeof(T).Name}.InvalidName",
+        description: $"{typeof(T).Name} name must be at least {DomainConstants.MinNameLength}" +
                      $" characters long and at most {DomainConstants.MaxNameLength} characters long.");
 
     public static Error MissingFirstName => Error.Validation(
-        code: $"{nameof(T)}.MissingFirstName",
+        code: $"{typeof(T).Name}.MissingFirstName",
         description: "First name is required.");
 
     public static Error InvalidFirstName => Error.Validation(
-        code: $"{nameof(T)}.InvalidFirstName",
+        code: $"{typeof(T).Name}.InvalidFirstName",
         description: $"First name must be at least {DomainConstants.MinNameLength}" +
                      $" characters long and at most {DomainConstants.MaxNameLength} characters long.");
 
     public static Error InvalidLastName => Error.Validation(
-        code: $"{nameof(T)}.InvalidLastName",
+        code: $"{typeof(T).Name}.InvalidLastName",
         description: $"Last name must be at least {DomainConstants.MinNameLength}" +
                      $" characters long and at most {DomainConstants.MaxNameLength} characters long.");
 
     public static Error MissingLastName => Error.Validation(
-        code: $"{nameof(T)}.MissingLastName",
+        code: $"{typeof(T).Name}.MissingLastName",
         description: "Last name is required.");
 
     public static Error MissingAddress => Error.Validation(
-        code: $"{nameof(T)}.MissingAddress",
-        description: $"The {nameof(T).ToLower()} has no address.");
+        code: $"{typeof(T).Name}.MissingAddress",
+        description: $"The {typeof(T).Name.ToLower()} has no address.");
 
     public static Error InvalidAddress => Error.Validation(
-        code: $"{nameof(T)}.InvalidAddress",
+        code: $"{typeof(T).Name}.InvalidAddress",
         description:
-        $"The {nameof(T).ToLower()} address must have at least {DomainConstants.MinAddressLength} characters" +
+        $"The {typeof(T).Name.ToLower()} address must have at least {DomainConstants.MinAddressLength} characters" +
         $" and at most {DomainConstants.MaxAddressLength} characters.");
 
     public static Error MissingEmailAddress => Error.Validation(
-        code: $"{nameof(T)}.MissingEmailAddress",
+        code: $"{typeof(T).Name}.MissingEmailAddress",
         description: "The email address is missing.");
 
     public static Error InvalidEmailAddress => Error.Validation(
-        code: $"{nameof(T)}.InvalidEmailAddress",
+        code: $"{typeof(T).Name}.InvalidEmailAddress",
         description: "The email address provided is invalid.");
 }
