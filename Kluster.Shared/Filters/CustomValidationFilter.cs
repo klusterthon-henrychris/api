21c76685b1d610329ef136a3eb0a91dbabd409bb
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-22 21:18:35 +0100

    standardize error from CustomValidationFIlter

--- Kluster.Shared/Filters/CustomValidationFilter.cs
+++ Kluster.Shared/Filters/CustomValidationFilter.cs
@@ -9,25 +9,22 @@ namespace Kluster.Shared.Filters
         public void OnActionExecuting(ActionExecutingContext context)
         {
             // Check if the model state is invalid
-            if (!context.ModelState.IsValid)
+            if (context.ModelState.IsValid) return;
+            
+            // Create a custom response
+            var customErrorResponse = new ApiErrorResponse<object>(
+                context.ModelState
+                    .Where(x => x.Value is { Errors.Count: > 0 })
+                    .SelectMany(kvp =>
+                        kvp.Value?.Errors.Select(e => new { Code = kvp.Key, Description = e.ErrorMessage }))
+                    .ToList(),
+                "One or more validation errors occured."
+            );
+                
+            context.Result = new ObjectResult(customErrorResponse)
             {
-                // Create a custom response
-                var customErrorResponse = new ApiErrorResponse<object>(
-                    context.ModelState
-                        .Where(x => x.Value is { Errors.Count: > 0 })
-                        .ToDictionary(
-                            kvp => kvp.Key,
-                            kvp => kvp.Value?.Errors.Select(e => e.ErrorMessage).ToArray()
-                        ),
-                    "Validation failed"
-                );
-
-                // Set the custom response as the result
-                context.Result = new ObjectResult(customErrorResponse)
-                {
-                    StatusCode = 400,
-                };
-            }
+                StatusCode = 400,
+            };
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
