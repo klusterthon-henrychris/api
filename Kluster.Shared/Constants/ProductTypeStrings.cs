8e9bef8343dbc657b835c28a80aa3408c232d95c
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-22 21:19:31 +0100

    add ProductType enum

--- Kluster.Shared/Constants/ProductTypeStrings.cs
+++ Kluster.Shared/Constants/ProductTypeStrings.cs
@@ -1,10 +1,16 @@
 ﻿namespace Kluster.Shared.Constants
 {
-    public static class ProductType
+    public static class ProductTypeStrings
     {
         public const string Physical = "Physical";
         public const string Digital = "Digital";
 
         public static string[] AllProductTypeOptions = [Physical, Digital];
+    }
+
+    public enum ProductType
+    {
+        Physical,
+        Digital
     }
 }
