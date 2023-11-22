b129be9d42a62b58ddf3db500b0907cc6d7ed40b
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-22 13:54:26 +0100

    add helpers for business validation rules

--- Kluster.BusinessModule/Validators/CreateBusinessRequestValidator.cs
+++ Kluster.BusinessModule/Validators/CreateBusinessRequestValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Kluster.BusinessModule.DTOs.Requests;
 using Kluster.BusinessModule.ServiceErrors;
+using Kluster.BusinessModule.Validators.Helpers;
 using Kluster.Shared.Domain;
 using Kluster.Shared.Validators;
 
@@ -13,34 +14,9 @@ public class CreateBusinessRequestValidator : AbstractValidator<CreateBusinessRe
         RuleFor(x => x).NotEmpty();
         RuleFor(x => x.BusinessName).ValidateName();
         RuleFor(x => x.BusinessAddress).ValidateAddress();
-
-        RuleFor(x => x.RcNumber)
-            .NotEmpty()
-            .WithMessage(Errors.Business.MissingRcNumber.Description)
-            .WithErrorCode(Errors.Business.MissingRcNumber.Code)
-            .MaximumLength(DomainConstants.MaxRcNumberLength)
-            .WithMessage(Errors.Business.InvalidRcNumber.Description)
-            .WithErrorCode(Errors.Business.InvalidRcNumber.Code);
-
-        RuleFor(x => x.CacNumber)
-            .NotEmpty()
-            .WithMessage(Errors.Business.MissingCacNumber.Description)
-            .WithErrorCode(Errors.Business.MissingCacNumber.Code)
-            .MaximumLength(DomainConstants.MaxCacNumberLength)
-            .WithMessage(Errors.Business.InvalidCacNumber.Description)
-            .WithErrorCode(Errors.Business.InvalidCacNumber.Code);
-
-        RuleFor(x => x.Industry)
-            .NotEmpty()
-            .WithMessage(Errors.Business.MissingIndustry.Description)
-            .WithErrorCode(Errors.Business.MissingIndustry.Code)
-            .MaximumLength(DomainConstants.MaxEnumLength)
-            .WithMessage(Errors.Business.InvalidIndustry.Description)
-            .WithErrorCode(Errors.Business.InvalidIndustry.Code);
-
-        RuleFor(x => x.BusinessDescription)
-            .MaximumLength(DomainConstants.MaxDescriptionLength)
-            .WithMessage(Errors.Business.InvalidDescription.Description)
-            .WithErrorCode(Errors.Business.InvalidDescription.Code);
+        RuleFor(x => x.RcNumber).ValidateRcNumber();
+        RuleFor(x => x.CacNumber).ValidateCacNumber();
+        RuleFor(x => x.Industry).ValidateIndustry();
+        RuleFor(x => x.BusinessDescription).ValidateBusinessDescription();
     }
 }
---
+++ Kluster.BusinessModule/Validators/Helpers/BusinessValidatorHelpers.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Kluster.BusinessModule.ServiceErrors;
+using Kluster.Shared.Domain;
+
+namespace Kluster.BusinessModule.Validators.Helpers;
+
+public static class BusinessValidatorHelpers
+{
+    public static void ValidateRcNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty()
+            .WithMessage(Errors.Business.MissingRcNumber.Description)
+            .WithErrorCode(Errors.Business.MissingRcNumber.Code)
+            .MaximumLength(DomainConstants.MaxRcNumberLength)
+            .WithMessage(Errors.Business.InvalidRcNumber.Description)
+            .WithErrorCode(Errors.Business.InvalidRcNumber.Code);
+    }
+
+    public static void ValidateCacNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty()
+            .WithMessage(Errors.Business.MissingCacNumber.Description)
+            .WithErrorCode(Errors.Business.MissingCacNumber.Code)
+            .MaximumLength(DomainConstants.MaxCacNumberLength)
+            .WithMessage(Errors.Business.InvalidCacNumber.Description)
+            .WithErrorCode(Errors.Business.InvalidCacNumber.Code);
+    }
+
+    public static void ValidateIndustry<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty()
+            .WithMessage(Errors.Business.MissingIndustry.Description)
+            .WithErrorCode(Errors.Business.MissingIndustry.Code)
+            .MaximumLength(DomainConstants.MaxEnumLength)
+            .WithMessage(Errors.Business.InvalidIndustry.Description)
+            .WithErrorCode(Errors.Business.InvalidIndustry.Code);
+    }
+
+    public static void ValidateBusinessDescription<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .MaximumLength(DomainConstants.MaxDescriptionLength)
+            .WithMessage(Errors.Business.InvalidDescription.Description)
+            .WithErrorCode(Errors.Business.InvalidDescription.Code);
+    }
+}
