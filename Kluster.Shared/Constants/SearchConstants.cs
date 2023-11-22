57cc20798af854cecc15f48d3edfcc67fefd2b07
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-22 16:21:01 +0100

    create searchConstants.cs

--- Kluster.Shared/Constants/SearchConstants.cs
+++ Kluster.Shared/Constants/SearchConstants.cs
@@ -1,6 +1,8 @@
 ﻿namespace Kluster.Shared.Constants;
 
-public class SearchConstants
+public static class SearchConstants
 {
-    
+    public const int PageSize = 20;
+    public const int PageNumber = 1;
+    public const string NotFound = "N/A";
 }
