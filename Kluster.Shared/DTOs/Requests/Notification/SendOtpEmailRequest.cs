b781979f48800202cb49a1e5fbdae4a82d4dcc4d
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-24 00:16:42 +0100

    Update SendOtpEmailRequest.cs

--- Kluster.Shared/DTOs/Requests/Notification/SendOtpEmailRequest.cs
+++ Kluster.Shared/DTOs/Requests/Notification/SendOtpEmailRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace Kluster.Shared.DTOs.Requests.Notification;
 
-public record SendOtpEmailRequest(string Otp, string FirstName, string LastName, string EmailAddress);
+public record SendOtpEmailRequest(string UserId, string Otp, string FirstName, string LastName, string EmailAddress);
