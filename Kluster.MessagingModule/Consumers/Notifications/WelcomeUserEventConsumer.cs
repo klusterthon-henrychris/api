c78fe5bce0fd75390b4a7d61ef27c24d24d9a578
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-24 00:15:25 +0100

    add event for welcomeUserEmails

---
+++ Kluster.MessagingModule/Consumers/Notifications/WelcomeUserEventConsumer.cs
@@ -0,0 +1,23 @@
+﻿using Kluster.Shared.MessagingContracts.Events.Notification;
+using Kluster.Shared.SharedContracts.NotificationModule;
+using MassTransit;
+
+namespace Kluster.Messaging.Consumers.Notifications;
+
+public class WelcomeUserEventConsumer(
+    INotificationService notificationService,
+    ILogger<WelcomeUserEventConsumer> logger) : IConsumer<WelcomeUserEvent>
+{
+    public async Task Consume(ConsumeContext<WelcomeUserEvent> context)
+    {
+        var success = await notificationService.SendWelcomeMail(context.Message.EmailAddress, context.Message.FirstName,
+            context.Message.LastName);
+        if (!success)
+        {
+            throw new Exception($"{nameof(WelcomeUserEvent)} failed for email: {context.Message.EmailAddress}");
+        }
+
+        // todo: don't log email address
+        logger.LogInformation($"Sent welcome email for: {context.Message.EmailAddress}.");
+    }
+}
---
+++ Kluster.Shared/MessagingContracts/Events/Notification/WelcomeUserEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Kluster.Shared.MessagingContracts.Events.Notification;
+
+public record WelcomeUserEvent(string EmailAddress, string FirstName, string LastName);
