9a8452540335547121217556424885489572c7ad
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-23 22:44:07 +0100

    remove attachment method

--- Kluster.NotificationModule/MailController.cs
+++ Kluster.NotificationModule/MailController.cs
@@ -9,20 +9,6 @@ namespace Kluster.NotificationModule;
 [AllowAnonymous]
 public class MailController(IMailService mailService) : BaseController
 {
-    [HttpPost("sendemailwithattachment")]
-    public async Task<IActionResult> SendMailWithAttachmentAsync([FromForm] MailDataWithAttachments mailData)
-    {
-        var result = await mailService.SendWithAttachmentsAsync(mailData, new CancellationToken());
-
-        if (result)
-        {
-            return Ok("Mail with attachment has successfully been sent.");
-        }
-
-        return StatusCode(StatusCodes.Status500InternalServerError,
-            "An error occured. The Mail with attachment could not be sent.");
-    }
-
     [HttpPost("sendmail")]
     public async Task<IActionResult> SendMailAsync([FromBody] MailData mailData)
     {
