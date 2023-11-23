60876cea02737d8b527d2a88f0b71a856bb4b6d5
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-23 18:24:19 +0100

    andd user endpoints to generate and confirm email otp

--- Kluster.UserModule/Controllers/UserController.cs
+++ Kluster.UserModule/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Kluster.Shared.API;
+using Kluster.Shared.Constants;
 using Kluster.Shared.DTOs.Requests.User;
 using Kluster.Shared.Extensions;
+using Kluster.Shared.SharedContracts.UserModule;
 using Kluster.UserModule.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,5 +27,20 @@ public class UserController(IUserService userService) : BaseController
     {
         var updateUserResult = await userService.UpdateUser(request);
         return updateUserResult.Match(_ => NoContent(), ReturnErrorResponse);
+    }
+
+    [HttpPost("confirm-email")]
+    public async Task<IActionResult> ConfirmEmailWithOtp(string userId, string otp)
+    {
+        var confirmEmailResult = await userService.ConfirmEmailWithOtp(userId, otp);
+        return confirmEmailResult.Match(_ => Ok(), ReturnErrorResponse);
+    }
+
+    [HttpPost("/{id}/new-otp")]
+    // route is email or phone
+    public async Task<IActionResult> GenerateNewOtp(string id, string otpRoute)
+    {
+        var generateNewOtpResult = await userService.GenerateNewOtp(id, otpRoute);
+        return generateNewOtpResult.Match(_ => Ok(), ReturnErrorResponse);
     }
 }
--- Kluster.UserModule/Services/UserService.cs
+++ Kluster.UserModule/Services/UserService.cs
@@ -1,17 +1,27 @@
 ﻿using ErrorOr;
+using Kluster.Shared.Constants;
+using Kluster.Shared.Domain;
 using Kluster.Shared.DTOs.Requests.User;
 using Kluster.Shared.DTOs.Responses.User;
 using Kluster.Shared.Exceptions;
 using Kluster.Shared.Extensions;
+using Kluster.Shared.MessagingContracts.Events;
 using Kluster.Shared.SharedContracts.UserModule;
 using Kluster.UserModule.Data;
 using Kluster.UserModule.ServiceErrors;
-using Kluster.UserModule.Services.Contracts;
 using Kluster.UserModule.Validators;
+using MassTransit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kluster.UserModule.Services;
 
-public class UserService(ICurrentUser currentUser, UserModuleDbContext context) : IUserService
+public class UserService(
+    ICurrentUser currentUser,
+    UserManager<ApplicationUser> userManager,
+    IBus bus,
+    ILogger<UserService> logger,
+    UserModuleDbContext context) : IUserService
 {
     public async Task<ErrorOr<UserResponse>> GetLoggedInUser()
     {
@@ -47,5 +57,55 @@ public class UserService(ICurrentUser currentUser, UserModuleDbContext context) 
         context.Update(user);
         await context.SaveChangesAsync();
         return Result.Updated;
+    }
+
+    public async Task<string> GenerateOtpForEmail(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user is null)
+        {
+            throw new UserNotFoundException();
+        }
+
+        var otp = await userManager.GenerateEmailConfirmationTokenAsync(user);
+        logger.LogInformation($"Generated OTP for {user.FirstName + " " + user.LastName} ");
+        return otp;
+    }
+
+    public async Task<ErrorOr<Success>> ConfirmEmailWithOtp(string userId, string otp)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user is null)
+        {
+            return Errors.User.NotFound;
+        }
+
+        var result = await userManager.ConfirmEmailAsync(user, otp);
+        if (result.Succeeded)
+        {
+            return Result.Success;
+        }
+
+        var errors = result.Errors
+            .Select(error => Error.Validation("User." + error.Code, error.Description))
+            .ToList();
+        return errors;
+    }
+
+    public async Task<ErrorOr<Success>> GenerateNewOtp(string id, string otpRoute)
+    {
+        var user = await userManager.FindByIdAsync(id);
+        if (user is null)
+        {
+            return Errors.User.NotFound;
+        }
+
+        if (otpRoute == OtpRoute.Email.ToString())
+        {
+            await bus.Publish(new EmailOtpRequestedEvent(user.FirstName, user.LastName, user.Email!, user.Id));
+            return Result.Success;
+        }
+
+        throw new NotImplementedException("SMS Validation not available.");
     }
 }
