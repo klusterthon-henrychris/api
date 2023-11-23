16b44bcf721551681020449456070499c310610d
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-23 18:25:00 +0100

    create event for sending email OTPs

---
+++ Kluster.MessagingModule/Consumers/Auth/EmailOtpRequestedEventConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Kluster.Shared.DTOs.Requests.Notification;
+using Kluster.Shared.MessagingContracts.Events;
+using Kluster.Shared.SharedContracts.NotificationModule;
+using Kluster.Shared.SharedContracts.UserModule;
+using MassTransit;
+
+namespace Kluster.Messaging.Consumers.Auth;
+
+public class EmailOtpRequestedEventConsumer(
+    IUserService userService,
+    INotificationService notificationService,
+    ILogger<EmailOtpRequestedEventConsumer> logger)
+    : IConsumer<EmailOtpRequestedEvent>
+{
+    public async Task Consume(ConsumeContext<EmailOtpRequestedEvent> context)
+    {
+        var otp = await userService.GenerateOtpForEmail(context.Message.UserId);
+        await notificationService.SendOtpEmail(new SendOtpEmailRequest(otp, context.Message.FirstName,
+            context.Message.LastName,
+            context.Message.EmailAddress));
+    }
+}
---
+++ Kluster.Shared/MessagingContracts/Events/EmailOtpRequestedEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Kluster.Shared.MessagingContracts.Events;
+
+public record EmailOtpRequestedEvent(string FirstName, string LastName, string EmailAddress, string UserId);
