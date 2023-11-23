b2d02bb767e48fb99ecab25c8453744ae4a4f45f
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-23 18:23:14 +0100

    Create UserNotFoundException.cs

---
+++ Kluster.Shared/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Kluster.Shared.Exceptions;
+
+/// <summary>
+/// Throws the message: The userId used for OTP generation is not linked to an existing user."
+/// </summary>
+/// <param name="message">Pass in a custom message or leave blank</param>
+public class UserNotFoundException(string message = "") :
+    Exception(string.IsNullOrEmpty(message)
+        ? "The userId used for OTP generation is not linked to an existing user."
+        : message
+    );
