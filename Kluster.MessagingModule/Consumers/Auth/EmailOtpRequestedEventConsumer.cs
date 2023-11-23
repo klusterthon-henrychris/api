c2c2df5fa025934953c7b4e6fb243d02ebca3142
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-24 00:16:57 +0100

    add logging to emailOtp consumer

--- Kluster.MessagingModule/Consumers/Auth/EmailOtpRequestedEventConsumer.cs
+++ Kluster.MessagingModule/Consumers/Auth/EmailOtpRequestedEventConsumer.cs
@@ -1,5 +1,5 @@
 ﻿using Kluster.Shared.DTOs.Requests.Notification;
-using Kluster.Shared.MessagingContracts.Events;
+using Kluster.Shared.MessagingContracts.Events.User;
 using Kluster.Shared.SharedContracts.NotificationModule;
 using Kluster.Shared.SharedContracts.UserModule;
 using MassTransit;
@@ -15,8 +15,16 @@ public class EmailOtpRequestedEventConsumer(
     public async Task Consume(ConsumeContext<EmailOtpRequestedEvent> context)
     {
         var otp = await userService.GenerateOtpForEmail(context.Message.UserId);
-        await notificationService.SendOtpEmail(new SendOtpEmailRequest(otp, context.Message.FirstName,
+        var success = await notificationService.SendOtpEmail(new SendOtpEmailRequest(context.Message.UserId, otp,
+            context.Message.FirstName,
             context.Message.LastName,
             context.Message.EmailAddress));
+
+        if (!success)
+        {
+            throw new Exception($"{nameof(EmailOtpRequestedEvent)} failed for user: {context.Message.UserId}");
+        }
+
+        logger.LogInformation($"Sent OTP email for: {context.Message.UserId}.");
     }
 }
