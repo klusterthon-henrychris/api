b47568211bdda7a081211b46dc1718ed5bf90b2d
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-23 18:23:22 +0100

    add otpRoute enum

---
+++ Kluster.Shared/Constants/OtpRoute.cs
@@ -0,0 +1,13 @@
+﻿namespace Kluster.Shared.Constants;
+
+public enum OtpRoute
+{
+    Email,
+    Phone
+}
+
+public static class OtpRouteStrings
+{
+    public const string Email = nameof(Email);
+    public const string Phone = nameof(Phone);
+}
