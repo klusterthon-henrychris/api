eccff0cdbb54f5830986286c4487295ce45036e9
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-25 10:03:43 +0100

    Create Wallet.cs

---
+++ Kluster.Shared/Domain/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kluster.Shared.Domain;
+
+public class Wallet
+{
+    public Wallet()
+    {
+        WalletId = "WAL-" + BusinessId;
+    }
+
+    [Key]
+    public string WalletId { get; set; }
+    public decimal Balance { get; set; }
+    public string Currency { get; set; } = "NGN";
+    public required string BusinessId { get; set; }
+    public Business Business { get; set; } = null!;
+}
