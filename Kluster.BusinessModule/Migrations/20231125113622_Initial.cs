1203722d14679a5e50a853ea00ac5f833edf92ad
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-25 12:45:03 +0100

    add new migrations

---
+++ Kluster.BusinessModule/Migrations/20231125113622_Initial.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kluster.BusinessModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "BusinessModule");
+
+            migrationBuilder.CreateTable(
+                name: "Businesses",
+                schema: "BusinessModule",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    RcNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Industry = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Businesses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                schema: "BusinessModule",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BusinessName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BusinessId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_Businesses_BusinessId",
+                        column: x => x.BusinessId,
+                        principalSchema: "BusinessModule",
+                        principalTable: "Businesses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "BusinessModule",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    ProductType = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    BusinessId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Businesses_BusinessId",
+                        column: x => x.BusinessId,
+                        principalSchema: "BusinessModule",
+                        principalTable: "Businesses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wallets",
+                schema: "BusinessModule",
+                columns: table => new
+                {
+                    WalletId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    BusinessId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallets", x => x.WalletId);
+                    table.ForeignKey(
+                        name: "FK_Wallets_Businesses_BusinessId",
+                        column: x => x.BusinessId,
+                        principalSchema: "BusinessModule",
+                        principalTable: "Businesses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_BusinessId",
+                schema: "BusinessModule",
+                table: "Clients",
+                column: "BusinessId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BusinessId",
+                schema: "BusinessModule",
+                table: "Products",
+                column: "BusinessId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wallets_BusinessId",
+                schema: "BusinessModule",
+                table: "Wallets",
+                column: "BusinessId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients",
+                schema: "BusinessModule");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "BusinessModule");
+
+            migrationBuilder.DropTable(
+                name: "Wallets",
+                schema: "BusinessModule");
+
+            migrationBuilder.DropTable(
+                name: "Businesses",
+                schema: "BusinessModule");
+        }
+    }
+}
---
+++ Kluster.PaymentModule/Migrations/20231125113652_Initial.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kluster.PaymentModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "PaymentModule");
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                schema: "PaymentModule",
+                columns: table => new
+                {
+                    InvoiceNo = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateOfIssuance = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    InvoiceItems = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    BillingAddress = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ClientId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    BusinessId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.InvoiceNo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                schema: "PaymentModule",
+                columns: table => new
+                {
+                    PaymentReference = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DateOfPayment = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OtherDetails = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    PaymentChannel = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    BusinessId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    InvoiceId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    ClientId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.PaymentReference);
+                    table.ForeignKey(
+                        name: "FK_Payments_Invoices_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalSchema: "PaymentModule",
+                        principalTable: "Invoices",
+                        principalColumn: "InvoiceNo",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_InvoiceId",
+                schema: "PaymentModule",
+                table: "Payments",
+                column: "InvoiceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments",
+                schema: "PaymentModule");
+
+            migrationBuilder.DropTable(
+                name: "Invoices",
+                schema: "PaymentModule");
+        }
+    }
+}
