712a9740a9307c5e291119717b71e5e1310e0f2e
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-25 23:45:55 +0100

    add paystack config

---
+++ Kluster.Shared/Configuration/PaystackSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Kluster.Shared.Configuration;
+
+public class PaystackSettings
+{
+    public string PublicKey { get; set; }
+    public string SecretKey { get; set; }
+    public string[] AllowedIps { get; set; }
+}
