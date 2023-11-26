82f327639f6426a391e0f64aea6703547ba78383
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-26 04:10:56 +0100

    add InitialInvoiceEmailFailed  error

--- Kluster.NotificationModule/ServiceErrors/Errors.Notification.cs
+++ Kluster.NotificationModule/ServiceErrors/Errors.Notification.cs
@@ -9,5 +9,9 @@ public static class Errors
         public static Error ForgotPasswordEmailFailed => Error.Failure(
             code: $"{nameof(Notification)}.ForgotPasswordEmailFailed",
             description: "Failed to send forgot password email.");
+        
+        public static Error InitialInvoiceEmailFailed => Error.Failure(
+            code: $"{nameof(Notification)}.InitialInvoiceEmailFailed",
+            description: "Failed to send initial invoice email.");
     }
 }
