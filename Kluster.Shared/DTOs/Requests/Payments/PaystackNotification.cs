3ffb0f7687a57517aeafd813ff7990e542c9e6b4
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-26 03:32:42 +0100

    update null warnings

--- Kluster.Shared/DTOs/Requests/Payments/PaystackNotification.cs
+++ Kluster.Shared/DTOs/Requests/Payments/PaystackNotification.cs
@@ -1,4 +1,6 @@
-﻿namespace Kluster.Shared.DTOs.Requests.Payments;
+﻿// ReSharper disable InconsistentNaming
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace Kluster.Shared.DTOs.Requests.Payments;
 
 public class PaystackNotification
 {
