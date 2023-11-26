24cd99decbc3feb2990bc22be6bbaacd0eb7858d
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-26 12:50:50 +0100

    setup serilog

--- Kluster.Host/Program.cs
+++ Kluster.Host/Program.cs
@@ -1,7 +1,19 @@
 using Kluster.Host;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterApplicationServices(builder.Environment);
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console(new JsonFormatter())
+    .WriteTo.Seq("http://localhost:5341")
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+);
 
 var app = builder.Build();
 app.ConfigureApplication();
