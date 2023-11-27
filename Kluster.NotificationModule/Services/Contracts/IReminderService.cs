818741e630cfd071b823b630e5fa85c1611cab36
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-27 21:13:48 +0100

    add function to send reminders

---
+++ Kluster.NotificationModule/Services/Contracts/IReminderService.cs
@@ -0,0 +1,8 @@
+﻿using Kluster.Shared.MessagingContracts.Events.Invoices;
+
+namespace Kluster.NotificationModule.Services.Contracts;
+
+public interface IReminderService
+{
+    Task SendInvoiceReminder(InvoiceCreatedEvent invoiceCreatedEvent);
+}
---
+++ Kluster.NotificationModule/Services/ReminderService.cs
@@ -0,0 +1,39 @@
+﻿using Hangfire;
+using Kluster.NotificationModule.Services.Contracts;
+using Kluster.Shared.Constants;
+using Kluster.Shared.DTOs.Requests.Invoices;
+using Kluster.Shared.MessagingContracts.Events.Invoices;
+using Kluster.Shared.SharedContracts.BusinessModule;
+using Kluster.Shared.SharedContracts.PaymentModule;
+using MassTransit;
+
+namespace Kluster.NotificationModule.Services;
+
+public class ReminderService(
+    IInvoiceService invoiceService,
+    IBusinessService businessService,
+    IBus bus,
+    ILogger<ReminderService> logger)
+    : IReminderService
+{
+    public async Task SendInvoiceReminder(InvoiceCreatedEvent invoiceCreatedEvent)
+    {
+        logger.LogInformation("Fetching invoice {0} to send reminder.", invoiceCreatedEvent.InvoiceId);
+        var invoice = await invoiceService.GetInvoiceInternal(invoiceCreatedEvent.InvoiceId);
+        if (invoice.Status == InvoiceStatus.Paid.ToString())
+        {
+            logger.LogInformation("Invoice {0} has been paid, ignoring reminder.", invoiceCreatedEvent.InvoiceId);
+
+            RecurringJob.RemoveIfExists(invoice.InvoiceNo);
+            return;
+        }
+
+        var businessName = await businessService.GetBusinessName(invoice.BusinessId) ?? "SimpleBiz";
+
+        await bus.Publish(new SendInvoiceReminderRequest(invoice.InvoiceNo, invoiceCreatedEvent.FirstName,
+            invoiceCreatedEvent.LastName, invoice.Amount, invoice.DueDate, invoice.DateOfIssuance,
+            invoice.ClientEmailAddress, invoice.Status,
+            businessName));
+        logger.LogInformation("Reminder mail request published for invoice {0}.", invoiceCreatedEvent.InvoiceId);
+    }
+}
