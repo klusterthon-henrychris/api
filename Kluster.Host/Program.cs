a547944e5053c7bf995580a0dcb24c4cd8596eb6
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-27 01:42:24 +0100

    add key vault

--- Kluster.Host/Program.cs
+++ Kluster.Host/Program.cs
@@ -1,10 +1,19 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Kluster.Host;
-using Kluster.Shared.Configuration;
-using Serilog;
-using Serilog.Events;
-using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+
+using var scope = builder.Services.BuildServiceProvider().CreateScope();
+var configuration = scope.ServiceProvider.GetService<IConfiguration>();
+
+var keyVaultName = configuration["KeyVault:Vault"];
+var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+var client = new SecretClient(new Uri(kvUri),
+    new DefaultAzureCredential(new DefaultAzureCredentialOptions
+        { ManagedIdentityClientId = configuration["KeyVault:ClientId"] }));
+var sec = client.GetSecret("KlusterApi-MailSettings--Password");
+Console.WriteLine($"Secret retrieved {sec}");
 builder.Services.RegisterApplicationServices(builder.Environment);
 builder.ConfigureSerilog();
 
