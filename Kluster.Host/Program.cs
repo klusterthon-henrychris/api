3e8fdd0181e7cf49e5d4fd50b3be08326300ca9b
henrychris <64085210+henrychris@users.noreply.github.com>
2023-11-27 14:15:45 +0100

    Update Program.cs

--- Kluster.Host/Program.cs
+++ Kluster.Host/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterApplicationServices();
 builder.ConfigureSerilog();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+Console.WriteLine($"PORT: {port}.");
+builder.WebHost.UseUrls($"http://*:{port}");
 var app = builder.Build();
 app.ConfigureApplication();
 app.Run();
